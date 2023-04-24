8c7919653a0f187edcae27e69db603f0379fae1b
Cristina Taboada <cris.taboada98@gmail.com>
2023-04-24 12:34:57 +0200

    practica completada

--- app_imdb/index.js
+++ app_imdb/index.js
@@ -1,54 +1,6 @@
-//-- Definición de clases
-class Movie {
-    //-- Constructor
-    constructor(title,realiseYear,nacionality,genre,photo){
-        this.title = title;
-        this.realiseYear = realiseYear;
-        this.actors = [];
-        this.nacionality = nacionality;
-        this.director = "";
-        this.writer = "";
-        this.language = "";
-        this.plataforma = "";
-        this.isMCU = false;
-        this.mainCharacterName = "";
-        this.producer = "";
-        this.distributor = "";
-        this.genre = genre;
-        this.photo = photo;
-    }
-}
-
-class Professional{
-    //-- Constructor
-    constructor(name,age,weight,height,isRetired,nationality,oscarsNumber,profession,photo){
-        this.name = name;
-        this.age = age;
-        this.weight = weight;
-        this.height = height;
-        this.isRetired = isRetired;
-        this.nationality = nationality;
-        this.oscarsNumber = oscarsNumber;
-        this.profession = profession;
-        this.photo = photo;
-    }
-}
-
-//------------------------------------------- Funcionalidades del Home -----------------------------------------
-//-- Funcion para ocultar todas las secciones --> funcionalidad del link Home.
-function ocultarTodo(){
-    $('#seccion-peliculas').css("display","none");
-    $('#seccion-profesionales').css("display","none");
-}
-
-function cargarDatos(){
-    cargarPeliculas();
-    cargarProfesionales();
-}
-
 //-------------------------------------------- Sección Películas ------------------------------------------------
 //-- Peliculas que siempre van a estar en la web.
-let pelicula1 = new Movie('Jumanji: Bienvenidos a la Jungla',2017,'Estados Unidos','Acción, aventura y comedia','https://www.pantalla90.es/wp-content/uploads/2017/12/1uQaSgtHyTN3r2fecL0mSs6geQO.jpg');
+const pelicula1 = new Movie('Jumanji: Bienvenidos a la Jungla',2017,'Estados Unidos','Acción, aventura y comedia','https://www.pantalla90.es/wp-content/uploads/2017/12/1uQaSgtHyTN3r2fecL0mSs6geQO.jpg');
 pelicula1.actors = ['Dwayne Johnson','Jack Black','Kevin Hart','Karen Gillan', 'Nick Jonas'];
 pelicula1.director = 'Jake Kasdan';
 pelicula1.writer = 'Chris Van Allsburg';
@@ -58,7 +10,7 @@ pelicula1.mainCharacterName = 'Spencer Gilpin';
 pelicula1.producer = 'Matthew Tolmach';
 pelicula1.distributor = 'Sony Pictures Releasing';
 
-let pelicula2 = new Movie('Parasite',2019,'Corea del Sur','Drama y thriller psicológico','https://pics.filmaffinity.com/Par_sitos-406070218-large.jpg');
+const pelicula2 = new Movie('Parasite',2019,'Corea del Sur','Drama y thriller psicológico','https://pics.filmaffinity.com/Par_sitos-406070218-large.jpg');
 pelicula2.actors = ['Song Kang-ho','Lee Sun-kyun','Cho Yeo-jeong','Choi Woo-shik','Park So-dam'];
 pelicula2.director = 'Bong Joon-ho';
 pelicula2.writer = 'Han Jin-won';
@@ -68,7 +20,7 @@ pelicula2.mainCharacterName = 'Kim Ki-taek';
 pelicula2.producer = 'Kwak Sin-ae';
 pelicula2.distributor = 'CJ Entertainment';
 
-let pelicula3 = new Movie('Spider-Man: No Way Home',2021,'Estados Unidos','Acción, aventura, ciencia ficción y superhéroes','https://pics.filmaffinity.com/Spider_Man_No_Way_Home-819449455-large.jpg');
+const pelicula3 = new Movie('Spider-Man: No Way Home',2021,'Estados Unidos','Acción, aventura, ciencia ficción y superhéroes','https://pics.filmaffinity.com/Spider_Man_No_Way_Home-819449455-large.jpg');
 pelicula3.actors = ['Tom Holland','Zendaya','Jacob Batalon','Marisa Tomei','Benedict Cumberbatch','Jon Favreau'];
 pelicula3.director = 'Jon Watts';
 pelicula3.writer = 'Chris McKenna';
@@ -89,54 +41,58 @@ function mostrarPeliculas() {
 }
 
 //-- Funcion de carga datos iniciales de la web
-function cargarPeliculas(){
-    for (let i = 0; i < registroPeliculas.length; i++) {
-        $('#peliculas').append(`
-            <div class="pelicula">
-                <!-- card -->
-                <div class="card" style="width: 18rem;">
-                    <img src="${registroPeliculas[i].photo}" class="card-img-top" alt="" id="card_img">
-                    <div class="card-body">
-                        <h5 class="card-title text-light" id="titulo">${registroPeliculas[i].title}</h5>
-                        <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#infoModal${i}">
-                            Más Info
-                        </button>
-                    </div>
+function htmlPelicula(i){
+    $('#peliculas').append(`
+        <div class="pelicula">
+            <!-- card -->
+            <div class="card" style="width: 18rem;">
+                <img src="${registroPeliculas[i].photo}" class="card-img-top" alt="" id="card_img">
+                <div class="card-body">
+                    <h5 class="card-title text-light" id="titulo">${registroPeliculas[i].title}</h5>
+                    <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#infoModal${i}">
+                        VER FICHA
+                    </button>
                 </div>
-                <!-- modal -->
-                <div class="modal fade" id="infoModal${i}" tabindex="-1" aria-labelledby="infoModalLabel" aria-hidden="true">
-                    <div class="modal-dialog modal-dialog-centered">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <h1 class="modal-title fs-5" id="titulo">${registroPeliculas[i].title}</h1>
-                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                            </div>
-                            <div class="modal-body" id="modal_info">
-                                <span class="fw-bold">Año:</span> ${registroPeliculas[i].realiseYear}<br>
-                                <span class="fw-bold">Reparto:</span> ${registroPeliculas[i].actors}<br>
-                                <span class="fw-bold">Nacionalidad:</span> ${registroPeliculas[i].nacionality}<br>
-                                <span class="fw-bold">Director:</span> ${registroPeliculas[i].director}<br>
-                                <span class="fw-bold">Escritor:</span> ${registroPeliculas[i].writer}<br>
-                                <span class="fw-bold">Idioma:</span> ${registroPeliculas[i].language}<br>
-                                <span class="fw-bold">¿MCU?:</span> ${registroPeliculas[i].isMCU}<br>
-                                <span class="fw-bold">Nombre del/la protagonista:</span> ${registroPeliculas[i].mainCharacterName}<br>
-                                <span class="fw-bold">Productor:</span> ${registroPeliculas[i].producer}<br>
-                                <span class="fw-bold">Distribuidor:</span> ${registroPeliculas[i].distributor}<br>
-                                <span class="fw-bold">Género:</span> ${registroPeliculas[i].genre}
-                            </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-dark" data-bs-dismiss="modal">Close</button>
-                            </div>
+            </div>
+            <!-- modal -->
+            <div class="modal fade" id="infoModal${i}" tabindex="-1" aria-labelledby="infoModalLabel" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h1 class="modal-title fs-5" id="titulo">${registroPeliculas[i].title}</h1>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body" id="modal_info">
+                            <span class="fw-bold">Año:</span> ${registroPeliculas[i].realiseYear}<br>
+                            <span class="fw-bold">Reparto:</span> ${registroPeliculas[i].actors}<br>
+                            <span class="fw-bold">Nacionalidad:</span> ${registroPeliculas[i].nacionality}<br>
+                            <span class="fw-bold">Director:</span> ${registroPeliculas[i].director}<br>
+                            <span class="fw-bold">Escritor:</span> ${registroPeliculas[i].writer}<br>
+                            <span class="fw-bold">Idioma:</span> ${registroPeliculas[i].language}<br>
+                            <span class="fw-bold">¿MCU?:</span> ${registroPeliculas[i].isMCU}<br>
+                            <span class="fw-bold">Nombre del/la protagonista:</span> ${registroPeliculas[i].mainCharacterName}<br>
+                            <span class="fw-bold">Productor:</span> ${registroPeliculas[i].producer}<br>
+                            <span class="fw-bold">Distribuidor:</span> ${registroPeliculas[i].distributor}<br>
+                            <span class="fw-bold">Género:</span> ${registroPeliculas[i].genre}
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-dark" data-bs-dismiss="modal">Close</button>
                         </div>
                     </div>
                 </div>
             </div>
-        `);
+        </div>
+    `);
+}
+
+function cargarPeliculas(){
+    for (let i = 0; i < registroPeliculas.length; i++) {
+        htmlPelicula(i);
     }
 }
 
 //-- Funcion para añadir peliculas mediante el formulario
-//-- contador del bucle que debe ir fuera del metodo para que cada vez que se añada una pelicula aumente una 
+//-- contador del bucle que debe ir fuera del metodo para que cada vez que se añada una pelicula aumente una
 //-- unidad sin resetearse.
 let i = 3;
 function añadirPelicula(){
@@ -169,47 +125,7 @@ function añadirPelicula(){
 
     //-- Mostrar en pantalla la pelicula añadida.
     for (i; i < registroPeliculas.length; i++) {
-        $('#peliculas').append(`
-            <div class="pelicula">
-                <!-- card -->
-                <div class="card" style="width: 18rem;">
-                    <img src="${registroPeliculas[i].photo}" class="card-img-top" alt="" id="card_img">
-                    <div class="card-body">
-                        <h5 class="card-title text-light" id="titulo">${registroPeliculas[i].title}</h5>
-                        <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#infoModal${i}">
-                            Más Info
-                        </button>
-                    </div>
-                </div>
-                <!-- modal -->
-                <div class="modal fade" id="infoModal${i}" tabindex="-1" aria-labelledby="infoModalLabel" aria-hidden="true">
-                    <div class="modal-dialog modal-dialog-centered">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <h1 class="modal-title fs-5" id="titulo">${registroPeliculas[i].title}</h1>
-                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                            </div>
-                            <div class="modal-body" id="modal_info">
-                                <span class="fw-bold">Año:</span> ${registroPeliculas[i].realiseYear}<br>
-                                <span class="fw-bold">Reparto:</span> ${registroPeliculas[i].actors}<br>
-                                <span class="fw-bold">Nacionalidad:</span> ${registroPeliculas[i].nacionality}<br>
-                                <span class="fw-bold">Director:</span> ${registroPeliculas[i].director}<br>
-                                <span class="fw-bold">Escritor:</span> ${registroPeliculas[i].writer}<br>
-                                <span class="fw-bold">Idioma:</span> ${registroPeliculas[i].language}<br>
-                                <span class="fw-bold">¿MCU?:</span> ${registroPeliculas[i].isMCU}<br>
-                                <span class="fw-bold">Nombre del/la protagonista:</span> ${registroPeliculas[i].mainCharacterName}<br>
-                                <span class="fw-bold">Productor:</span> ${registroPeliculas[i].producer}<br>
-                                <span class="fw-bold">Distribuidor:</span> ${registroPeliculas[i].distributor}<br>
-                                <span class="fw-bold">Género:</span> ${registroPeliculas[i].genre}
-                            </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-dark" data-bs-dismiss="modal">Close</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        `);
+        htmlPelicula(i);
     }
 
 }
@@ -234,44 +150,60 @@ function mostrarProfesionales() {
 }
 
 //-- Funcion de carga datos iniciales de la web
-function cargarProfesionales(){
-    for (let i = 0; i < registroProfesionales.length; i++) {
-        $('#profesionales').append(`
-            <div class="profesional">
-                <!-- card -->
-                <div class="card" style="width: 18rem;">
-                    <img src="${registroProfesionales[i].photo}" class="card-img-top" alt="" id="card_img">
-                    <div class="card-body">
-                        <h5 class="card-title text-light" id="titulo">${registroProfesionales[i].name}</h5>
-                        <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#infoProfModal${i}">
-                            Más Info
-                        </button>
-                    </div>
+function htmlProfesionales(i){
+    $('#profesionales').append(`
+        <div class="profesional">
+            <!-- card -->
+            <div class="card" style="width: 18rem;">
+                <img src="${registroProfesionales[i].photo}" class="card-img-top" alt="" id="card_img">
+                <div class="card-body">
+                    <h5 class="card-title text-light fs-4" id="titulo">${registroProfesionales[i].name}</h5>
+                    <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#infoProfModal${i}">
+                        MOSTRAR PERFIL
+                    </button>
                 </div>
-                <!-- modal -->
-                <div class="modal fade" id="infoProfModal${i}" tabindex="-1" aria-labelledby="infoModalLabel" aria-hidden="true">
-                    <div class="modal-dialog modal-dialog-centered">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <h1 class="modal-title fs-5" id="titulo">${registroProfesionales[i].name}</h1>
-                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                            </div>
-                            <div class="modal-body" id="modal_info">
-                                <span class="fw-bold">Edad:</span> ${registroProfesionales[i].age}<br>
-                                <span class="fw-bold">Peso:</span> ${registroProfesionales[i].weight}<br>
-                                <span class="fw-bold">Altura:</span> ${registroProfesionales[i].height}<br>
-                                <span class="fw-bold">¿Está retirado/a?:</span> ${registroProfesionales[i].isRetired}<br>
-                                <span class="fw-bold">Nacionalidad:</span> ${registroProfesionales[i].nationality}<br>
-                                <span class="fw-bold">Nº de Oscars ganados:</span> ${registroProfesionales[i].oscarsNumber}<br>
-                                <span class="fw-bold">Profesión:</span> ${registroProfesionales[i].profession}
-                            </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-dark" data-bs-dismiss="modal">Close</button>
-                            </div>
+            </div>
+            <!-- modal -->
+            <div class="modal fade" id="infoProfModal${i}" tabindex="-1" aria-labelledby="infoModalLabel" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h1 class="modal-title fs-5" id="titulo">${registroProfesionales[i].name}</h1>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body" id="modal_info">
+                            <span class="fw-bold">Edad:</span> ${registroProfesionales[i].age}<br>
+                            <span class="fw-bold">Peso:</span> ${registroProfesionales[i].weight}<br>
+                            <span class="fw-bold">Altura:</span> ${registroProfesionales[i].height}<br>
+                            <span class="fw-bold">¿Está retirado/a?:</span> ${registroProfesionales[i].isRetired}<br>
+                            <span class="fw-bold">Nacionalidad:</span> ${registroProfesionales[i].nationality}<br>
+                            <span class="fw-bold">Nº de Oscars ganados:</span> ${registroProfesionales[i].oscarsNumber}<br>
+                            <span class="fw-bold">Profesión:</span> ${registroProfesionales[i].profession}
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-dark" data-bs-dismiss="modal">Close</button>
                         </div>
                     </div>
                 </div>
             </div>
-        `);
+        </div>
+    `);
+}
+
+function cargarProfesionales(){
+    for (let i = 0; i < registroProfesionales.length; i++) {
+        htmlProfesionales(i);
     }
+}
+
+//------------------------------------------- Funcionalidades del Home -----------------------------------------
+//-- Funcion para ocultar todas las secciones --> funcionalidad del link Home.
+function ocultarTodo(){
+    $('#seccion-peliculas').css("display","none");
+    $('#seccion-profesionales').css("display","none");
+}
+
+function cargarDatos(){
+    cargarPeliculas();
+    cargarProfesionales();
 }
